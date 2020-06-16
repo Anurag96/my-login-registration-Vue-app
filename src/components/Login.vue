60a3b30787b1f5439e811fadc6a550db263da2a7
Anurag Kumar <anurag.kumar@wwt.com>
2020-06-16 09:26:15 -0500

    Added validation to registration page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,22 +4,22 @@
     <h1>Login</h1>
     
     <div class="box">
-       <form id="login-form">
+       <form id="login-form" @submit.prevent="addUser">
       <!-- name -->
       <div class="field">
         <label class="label">User Name </label><br>
-        <input type="text" class="input" name="userName">
+        <input type="text" class="input" v-model="userName" name="userName">
       </div>
 
       <!-- email -->
       <div class="field">
         <label class="label">Email </label> <br>
-        <input type="email" class="input" name="email">
+        <input type="email" class="input" v-model="email" name="email">
       </div>
 
       <div class="field">
         <label class="label">Password </label> <br>
-        <input type="password" class="input" name="password">
+        <input type="password" class="input" v-model="password" name="password">
       </div>
 
       <!-- submit button -->
@@ -37,10 +37,23 @@
 
 <script>
 import Header from './Header.vue'
+// import Registration from './Registration'
 
 export default {
   name: 'Login',
-  components:{Header}
+  components:{Header},
+  methods:{
+   addUser(){
+     
+   }
+  },
+  data(){
+    return{
+      userName:"",
+      email:"",
+      password:""
+    }
+  }
 
 }
 </script>
--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,91 +1,158 @@
 <template>
   <div>
-     <div><Header/></div>
+    <div>
+      <Header />
+    </div>
     <h1>Registration</h1>
-    
+
     <div class="box">
-       <form id="signup-form" @submit.prevent="addUser">
-  
-      <div class="field">
-        <label class="label">Username </label><br>
-        <input type="text" class="input" v-model="userName" name="userName" required>
-      </div>
+      <form id="signup-form" @submit.prevent="addUser">
+        <div class="field">
+          <label>
+            Username
+            <span class="eshtrik">*</span>
+          </label>
+          <br />
+          <input type="text"  class="input" v-model="userName" name="userName" required />
+        </div>
+        <span class="error" v-if="!validationArr.userName.valid">
+          <label v-for="msg in validationArr.userName.msgs" :key="msg">{{msg}}</label>
+        </span>
+        <div class="field">
+          <label>
+            Email ID
+            <span class="eshtrik">*</span>
+          </label>
+          <br />
+          <input type="email" class="input" v-model="email" name="email" required />
+          <br />
+          <span class="error" v-if="!validationArr.email.valid">
+            <label v-for="msg in validationArr.email.msgs" :key="msg">{{msg}}</label>
+          </span>
+        </div>
 
-      <div class="field">
-        <label class="label">Email ID </label><br>
-        <input type="email" class="input" v-model="email" name="email" required>
-      </div>
+        <div class="field">
+          <label>Password</label>
+          <br />
+          <input type="password" class="input" v-model="password" name="password" required />
+        </div>
 
-      <div class="field">
-        <label class="label">Password </label><br>
-        <input type="password" class="input" v-model="password" name="password" required>
-      </div>
+        <div class="field">
+          <label>Confirm Password</label>
+          <br />
+          <input type="password" class="input" v-model="passwordconfirm" name="passwordconfirm" />
+        </div>
+        <br />
 
-      <div class="field">
-        <label class="label">Confirm Password </label><br>
-        <input type="password" class="input" name="password_confirmation">
-      </div>
- <br>
-      <!-- submit button -->
-      <div class="field has-text-right">
-        <button type="submit" class="button is-danger">Submit</button><br>
-       <br> <router-link to="/login">Login |</router-link>
-         <router-link to="/about"> About</router-link>
-      </div>
-      <!-- ERRORS -->
-    <!-- <div class="alert alert-danger" v-show="errors.any()">
-      <div v-if="errors.has('password')">
-        {{ errors.first('password') }}
-      </div>
-      <div v-if="errors.has('password_confirmation')">
-        {{ errors.first('password_confirmation') }}
-      </div>
-    </div> -->
-    </form>
+        <span class="error" v-if="!validationArr.passwordconfirm.valid">
+          <label v-for="msg in validationArr.passwordconfirm.msgs" :key="msg">{{msg}}</label>
+        </span>
+        <!-- submit button -->
+        <div class="field has-text-right">
+          <button type="submit">Submit</button>
+          <br />
+          <br />
+          <router-link to="/login">Login |</router-link>
+          <router-link to="/about">About</router-link>
+        </div>
+      </form>
     </div>
   </div>
- 
 </template>
 
 <script>
-import Header from './Header.vue'
+import Header from "./Header.vue";
 import { uuid } from "vue-uuid";
-// import VeeValidate from "vee-validate";
-
 
 export default {
-  name: 'Registration',
-  components:{Header},
+  name: "Registration",
+  components: { Header },
   props: {
     msg: String
   },
-  data(){
-    return{
-      userName:"",
-      email:"",
-      password:""
-    
-    }
+  data() {
+    return {
+      userName: "",
+      email: "",
+      passwordconfirm: "",
+      password: "",
+            validationArr: {
+              userName: {
+                valid: true,
+                msgs: []
+              },
+              email: {
+                valid: true,
+                msgs: []
+              },
+              passwordconfirm: {
+                valid: true,
+                msgs: []
+              }
+      }
+    };
   },
   methods: {
-    addUser(){
-      const newTodoObj = {
+  addUser() {       
+      let users = JSON.parse(localStorage.getItem("users"));
+      let newTodoObj = [];
+      for(let key in this.validationArr){
+        this.validationArr[key]['valid']=true;
+        this.validationArr[key]['msgs']=[];
+      }
+
+      if (users) {
+        var isValidate = true;
+        let emailExists = users.filter(user => user.email == this.email);
+        let userExists = users.filter(user => user.userName == this.userName);
+   
+        if (emailExists.length) {
+          this.validationArr.email.valid = false;
+          this.validationArr.email.msgs = ["Email already exsits"];
+          isValidate =  false;
+        }  if (userExists.length) {
+          this.validationArr.userName.valid = false;
+          this.validationArr.userName.msgs = ["User already exsits"];
+          isValidate = false;
+        }  if (this.password!=='' && this.passwordconfirm!=='' && this.password !== this.passwordconfirm) {
+          this.validationArr.passwordconfirm.valid = false;
+          this.validationArr.passwordconfirm.msgs = ["Password doesnt match"];
+          isValidate = false;
+        } 
+
+        if(isValidate) {
+          users.push({
             id: uuid.v1(),
             userName: this.userName,
             email: this.email,
-            password:this.password
-          };
-    // localStorage.setItem('users',JSON.stringify())
-    // localStorage.getItem('abc','ff5')
-    // localStorage.removeItem('abc','ff5')
-    localStorage.setItem(newTodoObj.id,JSON.stringify())
-    localStorage.setItem(newTodoObj.userName,JSON.stringify())
-    localStorage.setItem(newTodoObj.email,JSON.stringify())
-    localStorage.setItem(newTodoObj.password,JSON.stringify())
-    // localStorage.setItem(newTodoObj.id,newTodoObj.userName,newTodoObj.email,newTodoObj.password,JSON.stringify())
+            password: this.password
+          });
+          newTodoObj = users;
+        } else{
+          return false
+        }
+
+      } else {
+        newTodoObj = [
+          {
+            id: uuid.v1(),
+            userName: this.userName,
+            email: this.email,
+            password: this.password
+          }
+        ];
+      }
+
+      localStorage.setItem("users", JSON.stringify(newTodoObj));
+
+      this.userName = "";
+      this.email = "";
+      this.passwordconfirm = "";
+      this.password = "";
+      
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -105,9 +172,17 @@ a {
 }
 field {
   float: left;
-width: 20%;
+  width: 20%;
 }
 box {
   margin: 1px;
+}
+
+span.error {
+  color: red;
+  font-size: 14px;
+}
+span.eshtrik {
+  color: red;
 }
 </style>
